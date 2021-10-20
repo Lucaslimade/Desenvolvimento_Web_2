668458ab13bc1ab204d6043b513212a95ac447fb
Lucas Lima <84357369+Lucaslimade@users.noreply.github.com>
2021-10-19 23:10:00 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = ""; //aqui eu não consegui recuperar minha senha de acesso ao BD, provalvelmente é por isso que não funciona
+$dbname = "cad";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
